0e7f7e5fa8f95fa6d23527b73f2976dc8cfd5706
Tom Madelin <tom.madelin@ibm.com@Toms-MacBook-Pro.local>
2020-01-03 14:45:55 +0000

    Add a mock service

--- client/src/components/StockItemList.jsx
+++ client/src/components/StockItemList.jsx
@@ -64,7 +64,7 @@ class StockItemList extends Component {
   async componentDidMount() {
 
     this.setState({
-      data: this.data,
+        data: await this.props.stockService.listStockItems()
     })
   }
 
